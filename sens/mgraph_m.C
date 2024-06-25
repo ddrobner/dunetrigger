b4f10014519ef48988bccd3f7961933d9907cb32
David Drobner <david@drobner.ca>
2024-06-25 11:25:58 -0400

    add equiv mip axis

--- sens/mgraph_m.C
+++ sens/mgraph_m.C
@@ -4,15 +4,21 @@
 #include "TCanvas.h"
 #include "TLegend.h"
 #include "TColor.h"
+#include "TGaxis.h"
 
 #include <vector>
 
 void mgraph_m(){
+    // argon work functions
+    const double w_ar = 23.6E-6; // mev per electron
+
     // lol using vector here to be able to iterate nicely
     std::vector<int> thresholds = {100, 150, 200, 250, 300};
 
     std::vector<TFile> file_refs;
     std::vector<TGraph*> graph_refs;
+    double mipaxis_min;
+    double mipaxis_max;
 
     gStyle->SetPalette(55);
 
@@ -53,5 +59,18 @@ void mgraph_m(){
     mg->GetXaxis()->SetLimits(0,25000);
     mg->GetYaxis()->SetLimits(0,1);
 
+    mipaxis_min = (mg->GetXaxis()->GetXmin())*w_ar;
+    mipaxis_max = (mg->GetXaxis()->GetXmax())*w_ar;
+
+    
+
+    TGaxis* mipAxis = new TGaxis(mg->GetXaxis()->GetXmin(), mg->GetYaxis()->GetXmax()+0.045, mg->GetXaxis()->GetXmax(), mg->GetYaxis()->GetXmax()+0.045, mipaxis_min, mipaxis_max, 510,"-");
+    mipAxis->SetTitle("Equivalent MIP (MeV)");
+    mipAxis->Draw();
+    mipAxis->SetTitleFont(42);
+    mipAxis->SetTitleSize(0.035);
+    mipAxis->SetLabelFont(42);
+    mipAxis->SetLabelSize(0.035);
+    mipAxis->SetLabelOffset(-0.0075);
     c1->SaveAs("test_mg.png");
 }
