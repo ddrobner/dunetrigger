a5a6820f93ba36bd0c9dc5d59f1db76238482b6b
David Drobner <david@drobner.ca>
2024-06-28 15:08:30 -0400

    reworking

--- dunetrigger/TriggerSim/TriggerCosmicSensitivityAnalysis_module.cc
+++ dunetrigger/TriggerSim/TriggerCosmicSensitivityAnalysis_module.cc
@@ -14,37 +14,45 @@
 #include "art/Framework/Principal/Handle.h"
 #include "art/Framework/Principal/Run.h"
 #include "art/Framework/Principal/SubRun.h"
-#include "art_root_io/TFileService.h"
-#include "art_root_io/TFileDirectory.h"
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
-#include "larcoreobj/SimpleTypesAndConstants/RawTypes.h"
 
-
-#include "lardataobj/RawData/RawDigit.h"
+// ART root IO
+//#include "art_root_io/TFileService.h"
+// ^^ don't need this for now
 
 // dune trigger structs
 #include "detdataformats/trigger/TriggerPrimitive.hpp"
 
+// Larsoft formats
 #include "lardataobj/Simulation/SimChannel.h"
+#include "lardataobj/RawData/RawDigit.h"
+#include "larcoreobj/SimpleTypesAndConstants/RawTypes.h"
+#include "EventStructs.hh"
 
+// ROOT classes
 #include "TGraph.h"
 #include "TCanvas.h"
 #include "TTree.h"
 #include "TBranch.h"
+#include "TMultiGraph.h"
+#include "TH1F.h"
 
 // I'm just saying screw it and ignoring what art/larsoft does usually here
 #include "TFile.h"
 
+// C++ stdlib stuff we need
 #include <stdint.h>
 #include <map>
 #include <cstdlib>
 #include <ranges>
+#include <filesystem>
 
 namespace duneana
 {
+  typedef std::pair<TDCTime_t, uint32_t> pulse_t;
   class TriggerCosmicSensitivityAnalysis;
 };
 
@@ -62,22 +70,12 @@ public:
   TriggerCosmicSensitivityAnalysis &operator=(TriggerCosmicSensitivityAnalysis &&) = delete;
 
   // Note to self: using a struct here would have been way easier than a tuple
-  typedef unsigned int TDCTime_t;
-
-  struct tpEvent {
-    TDCTime_t tdc_time;
-    uint32_t adc_int;
-    uint16_t adc_peak;
-  };
-
-  struct electronEvent {
-    TDCTime_t tdc_time;
-    float num_electrons;
-    float energy;
-  };
 
   // Required functions.
   void analyze(art::Event const &e) override;
+
+  void beginJob() override;
+  void endJob() override;
 
 private:
 
@@ -85,20 +83,19 @@ private:
   art::InputTag tp_tag_;
   art::InputTag tpc_tag_;
 
-  TTree* fTGTree;
-  TBranch* fPairBranch;
-
   unsigned int adc_threshold;
   unsigned int electron_pedestal;
 
   std::vector<dunedaq::trgdataformats::TriggerPrimitive> fTriggerPrimitives;
-  std::vector<uint32_t> fADCIntegrals;
 
   std::vector<sim::SimChannel> fSimChannels;
-  std::pair<std::vector<double>, std::vector<double>> paired_data;
 
   std::map<raw::ChannelID_t, std::vector<electronEvent>> electron_channels;
   std::map<uint32_t, std::vector<tpEvent>> tp_channels;
+
+  TGraph* tg;
+  TFile* o_file;
+
 
   // absolute difference for any unsigned datatype
   // necessary to avoid integer underflow
@@ -106,52 +103,9 @@ private:
     return (a > b) ? a - b : b - a;
   }
 
-  // writing some methods we will use in the analysis below
-  // TODO? templatize these to use on TPs/other data products?
-  std::vector<electronEvent> get_electron_pulse(std::vector<electronEvent>& channel_data, unsigned int start_time, unsigned int max_time){
-    std::vector<electronEvent> pulse_evts;
-    for(electronEvent evt : channel_data){
-      if(u_absdiff<unsigned int>(evt.tdc_time, start_time) <= max_time){
-        pulse_evts.push_back(evt);
-      }
-    }
-    return pulse_evts;
-  };
 
-  float find_pulse_peak(std::vector<electronEvent>& channel_data, TDCTime_t start_time, TDCTime_t max_time){
-    std::vector<electronEvent> e_pulse = get_electron_pulse(channel_data, start_time, max_time);
-    // todo figure out if vectors have nice facilities for this
-    unsigned int high = 0;
-    for (electronEvent i : e_pulse){
-      unsigned int val = i.num_electrons;
-      if(val > high){
-        high = val;
-      }
-    } 
-    return high;
-  };
-
-  std::vector<TDCTime_t> pulse_start_times(std::vector<electronEvent>& channel_data, TDCTime_t max_time){
-    std::vector<TDCTime_t> start_times = {channel_data.at(0).tdc_time,};
-    for(size_t i = 0; i < (channel_data.size()-1); i++){
-      if( ( ( u_absdiff<unsigned int>(start_times.back(), channel_data[i].tdc_time) >= max_time ) || u_absdiff<unsigned int>(channel_data[i].tdc_time, channel_data[i+1].tdc_time) >= max_time ) & ( u_absdiff<unsigned int>(start_times.back(), channel_data[i].tdc_time) >= max_time)){
-        start_times.push_back(channel_data[i].tdc_time);
-      } 
-    }
-    return start_times;
-  }
-
-  uint32_t count_e_over_threshold(std::vector<electronEvent>& channel_data, TDCTime_t max_time, uint32_t threshold) {
-    uint32_t e_over_threshold = 0U;
-    std::vector<TDCTime_t> pulse_starts = pulse_start_times(channel_data, max_time);
-    for (TDCTime_t t_start : pulse_starts){
-      float e_peak = find_pulse_peak(channel_data, t_start, max_time);
-      if (e_peak >= static_cast<float>(threshold)){
-        e_over_threshold++;
-      }
-    }
-    return e_over_threshold;
-  }
+  std::vector<pulse_t> channel_pulse_charges(std::vector<electronEvent>& channel_data, uint32_t threshold);
+  
 
 };
 
@@ -167,6 +121,29 @@ duneana::TriggerCosmicSensitivityAnalysis::TriggerCosmicSensitivityAnalysis(fhic
   consumes<std::vector<dunedaq::trgdataformats::TriggerPrimitive>>(tp_tag_);
   consumes <std::vector<sim::SimChannel>>(tpc_tag_);
 }
+
+void duneana::TriggerCosmicSensitivityAnalysis::beginJob(){
+  //TGraph* tg = new TGraph();
+  tg = new TGraph();
+  //std::unique_ptr<TFile> o_file(TFile::Open("sens_hist.root", "UPDATE"));
+  //TFile* o_file(TFile::Open("sens_hit.root", "UPDATE"));
+  o_file = TFile::Open("sens_hist.root", "UPDATE");
+  std::ostringstream tg_title;
+  tg_title << adc_threshold << " ADC;Num Electrons;Efficiency";
+  tg->SetTitle(tg_title.str().c_str());
+  tg->SetLineColor(2);
+  tg->SetLineWidth(8);
+}
+
+void duneana::TriggerCosmicSensitivityAnalysis::endJob(){
+  tg->Draw("AL");
+  std::ostringstream tgname;
+  tgname << "TG_" << adc_threshold;
+  o_file->cd();
+  tg->Write(tgname.str().c_str());
+  o_file->Close();
+}
+
 
 void duneana::TriggerCosmicSensitivityAnalysis::analyze(art::Event const &e)
 {
@@ -191,7 +168,6 @@ void duneana::TriggerCosmicSensitivityAnalysis::analyze(art::Event const &e)
     adc_peak = i.adc_peak;
     channel = i.channel;
 
-    //tpEvent_t this_tp = {static_cast<TDCTime_t>(adc_time), adc_int, adc_peak};
     tpEvent this_tp;
     this_tp.tdc_time = static_cast<TDCTime_t>(adc_time); 
     this_tp.adc_int = adc_int;
@@ -230,58 +206,108 @@ void duneana::TriggerCosmicSensitivityAnalysis::analyze(art::Event const &e)
     }
   }
 
-  size_t num_tps = fTriggerPrimitives.size();
-
-
-  std::vector<double> electron_thresholds;
-  for(uint32_t i = 0; i < 40000; i += 500){
-    electron_thresholds.push_back(static_cast<double>(i));
-  }
-
-  std::vector<double> eff_per_t;
-  for (uint32_t thresh : electron_thresholds){
-    uint32_t c_el_over_t = 0;
-    for(const auto& sm_pair : electron_channels){
-      auto cur_data = sm_pair.second;
-      if(cur_data.size() != 0) {
-        c_el_over_t += count_e_over_threshold(cur_data, static_cast<TDCTime_t>(75), thresh);
+  // ok - so we compute total pulse charge based on that method
+  uint32_t electron_threshold = 1;
+  std::vector<std::pair<float, bool>> hit_res;
+  TDCTime_t tp_window = 100;
+  for(auto& sm_pair : electron_channels){
+    raw::ChannelID_t chan = sm_pair.first;
+    std::vector<electronEvent> chan_data = sm_pair.second;
+    //std::cout << "===================== CHANNEL " << chan << " ========================" << std::endl;
+    std::vector<pulse_t> pulses = channel_pulse_charges(chan_data, electron_threshold);
+    for(auto& p : pulses){
+      TDCTime_t p_time = p.first;
+      uint32_t num_electrons = p.second;
+      bool has_tp = false;
+      for(tpEvent t : tp_channels[chan]){
+        if(u_absdiff(p_time, t.tdc_time) < tp_window){
+          has_tp = true;
+          break;
+        } 
+      }
+      std::pair<float,bool> this_res(num_electrons, has_tp);
+      hit_res.push_back(this_res);
       }
     }
 
-    double ratio = static_cast<double>(num_tps)/static_cast<double>(c_el_over_t);
-    if (ratio >= 1.0){
-      eff_per_t.push_back(1.0);
-    } else{
-      eff_per_t.push_back(ratio);
-    }
+  std::vector<float> e_bin_edges;
+  int max_electrons = 100000;
+  int bin_size = 6250;
+  for(int i = 0; i < max_electrons; i+= bin_size){
+    e_bin_edges.push_back(static_cast<float>(i));
   }
 
-  std::ostringstream tg_title;
-  tg_title << adc_threshold << " ADC;Electron Threshold;Efficiency";
-  //TCanvas* c1 = new TCanvas();
-  auto tg = new TGraph(electron_thresholds.size(), electron_thresholds.data(), eff_per_t.data());
-  tg->SetTitle(tg_title.str().c_str());
-  tg->SetLineColor(2);
-  tg->SetLineWidth(3);
-  tg->SetMarkerSize(1.25);
-  tg->SetMarkerStyle(21);
-  tg->SetMarkerColor(1);
-  tg->Draw("AC");
-  //c1->SaveAs("test_tgraph4.png");
+  std::vector<std::pair<float, float>> bin_results;
 
-  std::ostringstream tg_outfname;
-  tg_outfname << adc_threshold << "_sens.root";
-  std::ostringstream tg_name;
-  tg_name << "TG_" << adc_threshold;
+  float last_eff = 0.0f;
+  // now let's compute the ratios for each bin
+  for(size_t i = 1; i<e_bin_edges.size(); i++){
+    float bin_low = e_bin_edges.at(i-1);
+    float bin_high = e_bin_edges.at(i);
 
-  std::unique_ptr<TFile> o_file(TFile::Open(tg_outfname.str().c_str(), "RECREATE"));
-  tg->Write(tg_name.str().c_str());
-  //o_file->WriteObject(&tg, tg_name.str().c_str());
-  o_file->Close();
+    float hits_w_tp = 0.0f;
+    float total_hits = 0.0f;
 
+    for(std::pair<float, bool> p: hit_res){
+      float num_es = p.first;
+      bool has_tp = p.second;
+      if( (num_es >= bin_low) && (num_es < bin_high) ){
+        total_hits += 1.0f;
+        if(has_tp){
+          hits_w_tp += 1.0f;
+        }
+      }
+    }
+    float eff = hits_w_tp/total_hits;
+    // check for NaN
+    if(eff != eff){
+      eff = last_eff;
+    } else {
+      eff = hits_w_tp/total_hits;
+    }
+   last_eff = eff;
+    float bin_mid = static_cast<float>(e_bin_edges.at(i-1)) + static_cast<float>(bin_size)/2;
+    std::pair<float, float> this_result(bin_mid, eff);
+    //std::cout << "MID: " << bin_mid << " EFF: " << eff << std::endl;
+    bin_results.push_back(this_result);
+  }
 
-  //std::cout << "Efficiency: " << static_cast<double>(num_tps)/static_cast<double>(total_e_over_thresh) << std::endl;
+  for(std::pair<float, float> r : bin_results){
+    tg->AddPoint(r.first, r.second);
+  }
+}
 
+std::vector<duneana::pulse_t> duneana::TriggerCosmicSensitivityAnalysis::channel_pulse_charges(std::vector<electronEvent>& channel_data, uint32_t threshold){
+  std::vector<pulse_t> pulse_info;
+  uint32_t hit_t = threshold;
+  bool last_over = false;
+  uint32_t total_charge = 0;
+  TDCTime_t start_time;
+  for(electronEvent e : channel_data){
+    uint32_t current_charge = e.num_electrons;
+    //hit_t = static_cast<uint32_t>(current_charge/2);
+    bool is_over = (current_charge > hit_t);
+    if(is_over){
+      if(last_over){
+        total_charge += current_charge;
+      }else if(!last_over){
+        total_charge += current_charge;
+        start_time = e.tdc_time;
+      }
+      last_over = true;
+    } else if(last_over && !is_over){
+      pulse_t this_pulse(start_time, total_charge);
+      pulse_info.push_back(this_pulse);
+      //std::cout << "START: " << start_time << " CHARGE: " << total_charge << std::endl;
+      total_charge = 0;
+      last_over = false;
+      //hit_t = threshold;
+    } else{
+      last_over = false;
+      //hit_t = threshold;
+    }
+  }
+  return pulse_info;
 }
 
 DEFINE_ART_MODULE(duneana::TriggerCosmicSensitivityAnalysis)
