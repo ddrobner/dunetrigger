6ac227f60064d669ad14883bb0af4368ac9ef046
David Drobner <david@drobner.ca>
2024-07-16 14:15:39 -0400

    Clean up my branch

--- dunetrigger/TriggerSim/ElectronDumper_module.cc
+++ dunetrigger/TriggerSim/ElectronDumper_module.cc
@@ -16,10 +16,13 @@
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
+#include "detdataformats/trigger/TriggerPrimitive.hpp"
 
 #include "larcore/CoreUtils/ServiceUtil.h"
 #include "larcoreobj/SimpleTypesAndConstants/geo_types.h"
 #include "larcore/Geometry/Geometry.h"
+
+#include "EventStructs.hh"
 
 #include "lardataobj/Simulation/SimChannel.h"
 #include "lardataobj/RawData/RawDigit.h"
@@ -48,6 +51,8 @@ public:
 
   // Required functions.
   void analyze(art::Event const& e) override;
+  void beginJob() override;
+  void endJob() override;
 
 private:
 
@@ -58,9 +63,20 @@ private:
 
   art::ServiceHandle<geo::Geometry> geom;
   // Declare member data here.
+  std::ofstream electron_outfile;
+  std::ofstream tps_outfile;
 
 };
 
+void duneana::ElectronDumper::beginJob(){
+  electron_outfile.open("electrons_mu_serialized_one.txt");
+  tps_outfile.open("tps_serialized_one.txt");
+}
+
+void duneana::ElectronDumper::endJob(){
+  electron_outfile.close();
+  tps_outfile.close();
+}
 
 duneana::ElectronDumper::ElectronDumper(fhicl::ParameterSet const& p)
   : EDAnalyzer{p},
@@ -77,9 +93,13 @@ duneana::ElectronDumper::ElectronDumper(fhicl::ParameterSet const& p)
 void duneana::ElectronDumper::analyze(art::Event const& e)
 {
   // Implementation of required member function here.
-  std::ofstream outfile;
-  outfile.open("electrons_mu_serialized.txt");
   std::vector<sim::SimChannel> els = *(e.getValidHandle<std::vector<sim::SimChannel>>(electron_tag));
+  std::vector<dunedaq::trgdataformats::TriggerPrimitive> tps = *(e.getValidHandle<std::vector<dunedaq::trgdataformats::TriggerPrimitive>>(tp_tag));
+
+  std::map<raw::ChannelID_t, std::vector<tpEvent>> tp_channels = SensUtilityFunctions::process_tps_into_map(tps);
+
+
+
   for(sim::SimChannel ec : els){
     raw::ChannelID_t chan = ec.Channel();
     //auto chan_rop = geom->ChannelToROP(chan);
@@ -89,12 +109,16 @@ void duneana::ElectronDumper::analyze(art::Event const& e)
       for(sim::TDCIDE tdcide : ec.TDCIDEMap()){
         TDCTime_t ts = tdcide.first;
         for(sim::IDE ide : tdcide.second){
-          outfile << "(" << chan << "," << ts << "," << ide.numElectrons << ")" << std::endl;
+          electron_outfile << "(" << chan << "," << ts << "," << ide.numElectrons << ")" << std::endl;
+        }
+        // outputting tp channel, time, peak time, peak adc, int adc 
+        for(tpEvent t : tp_channels[chan]){
+          tps_outfile << "(" << chan << "," << t.tdc_time << "," << t.peak_time << "," << t.adc_peak << "," << t.adc_int << std::endl; 
         }
       }
     }
   }
-  outfile.close();
+
 }
 
 DEFINE_ART_MODULE(duneana::ElectronDumper)
